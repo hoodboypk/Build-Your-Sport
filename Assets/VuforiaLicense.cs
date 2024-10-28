20ecd591b92e526edce95048f66272bc41aed14b
Pratik Kumar <pratik.k2004@gmail.com>
2024-10-28 15:41:05 +0530

    Shader Updates

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVdneUloei8vLy8vQUFBQm1hOXQ5YllMbkV1MHFXd05mZmdsOVY2RXlQR0JwZU0wVFd4RHF3MlJ0SjBWbmQxSEdpbUEwZDdIRUlhOHBDeFI4T09IOXFybGdhVDNQNmNuUkdQWlVYcmJIT3dKdDRvdFJwMWNSbFBvbkc1NWRUZ2NTbDZNWm5SbFY5LzhxSUNyWUZTNHFGR3I4aStZSnNyTUlibGJKdUQ5QjJpMFZETCtoNTRYZHNFdkZqUUo3THVkeG0xTVAxZllRSlAzZWgwMDdaWEh3cjVGellWS3JWZUJBalZadzJZUWhiTWpYaGJXNHVIU3NBaW9lQlhHTlNzS3Z4RytMV1lNeElGdTViN3c1b1JPT0E3dEY0ZTRyM3hHb1VlOGt3YW1xcVpIeGRKSG4wOVo3KzlHdk5WL25VdW5tYytZQnpuMWdpTk16Vk41V2VTcURWcUFIWVlISUU4bDhqUEIvWnJCcFRvVDVneC8rQWt5ak9CZ0FCQmw=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
